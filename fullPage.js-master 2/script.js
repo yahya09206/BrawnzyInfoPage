92d1730c4d0cc876726f1ce835ac193af09f6a52
Yahya Hussein <yahyahussein91@gmail.com>
2015-12-17 20:19:17 -0800

    got animations working

---
+++ fullPage.js-master 2/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ $('#fullpage').fullpage({
+ 	navigation: true,
+ 	// continuousVertical: false,
+
+ 	// afterRender: function(){
+ 	// 	setInterval(function() {
+ 	// 		$.fn.fullpage.moveSectionDown();
+ 	// 	}, 3000);
+ 	// }
+	});
+});
